4d661df46e959247efa944fa2f5ef1d4cd0dba59
Abenyy <abenezer.tariku2015@gmail.com>
2018-05-16 09:22:52 +0300

    davv

---
+++ demo.java
@@ -0,0 +1,12 @@
+class demo{
+	public static void main(){
+		system.out.println("bhcjdsgjbsfh");
+		system.out.println("bhcjdsgjbsfh");
+		system.out.println("bhcjdsgjbsfh");
+		system.out.println("bhcjdsgjbsfh");
+		system.out.println("bhcjdsgjbsfh");
+		
+		
+		
+		
+}
